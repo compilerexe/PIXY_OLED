644829c418e49e9c76bc7e6c322e3d6840e3eeef
Compiler Exe <compilerexe@gmail.com>
2015-09-23 00:34:42 +0700

    delete line not use.

--- Arduino_Pixy_Upgrade/Arduino_Pixy_Upgrade.ino
+++ Arduino_Pixy_Upgrade/Arduino_Pixy_Upgrade.ino
@@ -11,54 +11,9 @@ int getX, Left, Right;
 int getY;
 int time , error;
 
-class ServoLoop
-{
-public:
-  ServoLoop(int32_t pgain, int32_t dgain);
-
-  void update(int32_t error);
-   
-  int32_t m_pos;
-  int32_t m_prevError;
-  int32_t m_pgain;
-  int32_t m_dgain;
-};
-
-
-ServoLoop panLoop(300, 500);
-ServoLoop tiltLoop(500, 700);
-
-ServoLoop::ServoLoop(int32_t pgain, int32_t dgain)
-{
-  m_pos = PIXY_RCS_CENTER_POS;
-  m_pgain = pgain;
-  m_dgain = dgain;
-  m_prevError = 0x80000000L;
-}
-
-void ServoLoop::update(int32_t error)
-{
-  long int vel;
-  char buf[32];
-  if (m_prevError!=0x80000000)
-  {	
-    vel = (error*m_pgain + (error - m_prevError)*m_dgain)>>10;
-    //sprintf(buf, "%ld\n", vel);
-    //Serial.print(buf);
-    m_pos += vel;
-    if (m_pos>PIXY_RCS_MAX_POS) 
-      m_pos = PIXY_RCS_MAX_POS; 
-    else if (m_pos<PIXY_RCS_MIN_POS) 
-      m_pos = PIXY_RCS_MIN_POS;
-  }
-  m_prevError = error;
-}
-
-
-
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT); //left
   pinMode(12, OUTPUT); //right
@@ -75,8 +30,6 @@ void loop()
 { 
   
   uint16_t blocks;
-  char buf[32]; 
-  int32_t panError, tiltError;
   
   blocks = pixy.getBlocks();
   
@@ -85,18 +38,10 @@ void loop()
     
     time = 100;
     
-    panError = X_CENTER-pixy.blocks[0].x;
-    tiltError = pixy.blocks[0].y-Y_CENTER;
-    
-    panLoop.update(panError);
-    tiltLoop.update(tiltError);
-    
-    pixy.setServos(panLoop.m_pos, tiltLoop.m_pos);
     getX = pixy.blocks[0].x; getY = pixy.blocks[0].y;
           
 //    Serial.print("x: "); Serial.print(getX); Serial.print(" ");
 //    Serial.print("y: "); Serial.println(getY);
-    
     
     error = 150 - getX;
     
